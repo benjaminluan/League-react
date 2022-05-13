1ad507db25c73ca053ba065929f68f234774fd8f
benjaminluan <benjaminluan1@gmail.com>
2022-05-12 22:53:43 -0400

    edits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Champions from '../ui/Champions';
 import Landing from '../ui/Landing';
 
 const Home = () => {
     return (
         <>
         <Landing />
+        <Champions />
         </>
     )
 }
