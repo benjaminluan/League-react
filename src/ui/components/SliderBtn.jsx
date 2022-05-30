40fe8be0508cc9ef092f9ea70d66a28adab46c7d
benjaminluan <benjaminluan1@gmail.com>
2022-05-30 15:11:28 -0400

    Edits

--- src/ui/components/SliderBtn.jsx
+++ src/ui/components/SliderBtn.jsx
@@ -5,11 +5,9 @@ import { FontAwesomeIcon  } from "@fortawesome/react-fontawesome";
 
 const SliderBtn = ( { nextSlide, prevSlide } ) => {
     return (
-        <div className="slider__btn--container">
-            
+        <div className="slider__btn--container">      
             <FontAwesomeIcon icon="arrow-left"  onClick = {prevSlide} />
             <FontAwesomeIcon icon="arrow-right" onClick = {nextSlide}/>
-           
         </div>
     );
 }
